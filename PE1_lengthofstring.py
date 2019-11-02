38fb31e29665f116f5cd32d90e74b5e2d993701c
unknown <rrgurjar@gmail.com>
2019-11-02 12:43:50 +0530

    20191102CommitLength

--- PE1_lengthofstring.py
+++ PE1_lengthofstring.py
@@ -1,8 +1,20 @@
+# Count length of string & occurrence of each character in string
+
 # User inputs the string and it gets stored in variable str
-str = input("Enter a string: ")
+str1 = input("Enter a string: ")
 
 # counter variable to count the character in a string
 counter = 0
-for s in str:
+for s in str1:
       counter = counter+1
 print("Length of the input string is:", counter)
+
+char_freq = {} 
+  
+for i in str1: 
+    if i in char_freq: 
+        char_freq[i] += 1
+    else: 
+        char_freq[i] = 1
+
+print ("Count of each character in given string is:\n " + str (char_freq))
